95b701a1b153974310d638d6d03cdf48187b24bf
u6102798@anu.edu.au <u6102798@anu.edu.au>
2021-06-04 23:27:47 +1000

    Allowed relative path, also now interpreter_brainfuck.py is a valid brainfuck program.

--- interpreter_brainfuck/__init__.py
+++ interpreter_brainfuck/__init__.py
@@ -1,3 +1,4 @@
-from .Brainfuck_Classes import Brainfuck
+from .Brainfuck_Classes import Brainfuck, One_Dimensional_Brainfuck
+from .interpreter_brainfuck import main
 #from .Brainfuck_Classes import Two_Dimensional_Brainfuck
 #from .Brainfuck_Classes import n_Dimensional_Brainfuck
